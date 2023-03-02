93c1fb76e843538d1893455f0269c0332cdabd33
romank444 <romank444@gmail.com>
2023-03-02 17:14:10 +0200

    Hello teacher! It`s my calculator. V.01

---
+++ my calculator.py
@@ -0,0 +1,21 @@
+class Calculator():
+    def sum(self, a, b):
+        c = print(a+b)
+        return c
+
+    def substraction(self, a, b):
+        c = print(a-b)
+        return c
+
+    def multiply(self, a, b):
+        c= print(a*b)
+        return c
+
+    def divide(self, a, b):
+        c = print(a/b)
+        return c
+
+if __name__ == '__main__':
+    Calculator().multiply(5, 2)
+
+
