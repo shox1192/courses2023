5ffef828589c6a7cc2420cb355439d4b03da7113
romank444 <romank444@gmail.com>
2023-03-10 17:30:26 +0200

    Home task 1 (V 1.0).

---
+++ My  home tasks/Home task 1 V1.0.py
@@ -0,0 +1,16 @@
+class Calculator():
+    def multiply(self, a, b):
+        c = print(a * b)
+        return c
+
+
+if __name__ == '__main__':
+    valuea = int(input('Input value a'))
+    a = valuea
+
+    valueb = int(input('Input value b'))
+    b = valueb
+
+    valuec = valuea * valueb
+    c = valuec
+    print(c)
